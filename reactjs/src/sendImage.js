8e6b6ba507931984c135b5a80704ea624e5b61f0
zahidalidev <m.zahidalidev@gmail.com>
2021-01-06 04:46:29 +0500

    uploading mulitple files usign FormData

--- reactjs/src/sendImage.js
+++ reactjs/src/sendImage.js
@@ -4,21 +4,51 @@ import axios from 'axios';
 class SendImage extends Component {
 
     state = {
-        image: null
+        images: []
     };
 
-    handleImageChange = (e) => {
-        this.setState({
-            image: e.target.files[0]
-        })
+    handleImageChange = (event) => {
+        // this.setState({
+        //     images: e.target.files
+        // })
+
+        let images = []
+        for (var i = 0; i < event.target.files.length; i++) {
+            images[i] = event.target.files.item(i);
+        }
+        images = images.filter(image => image.name.match(/\.(jpg|jpeg|png|gif)$/))
+        let message = `${images.length} valid image(s) selected`
+
+        console.log(images)
+        this.setState({ images })
     };
 
     handleSubmit = async (e) => {
         e.preventDefault();
 
+        // const uploaders = this.state.images.map(async (image) => {
+        //     const data2 = new FormData();
+        //     data2.append("file", image, image.name);
+        //     // Make an AJAX upload request using Axios
+        //     const data = await axios.post('http://127.0.0.1:5000/trainModel', data2);
+
+        //     return data;
+        // });
+        // // Once all the files are uploaded 
+        // try {
+        //     const data = await axios.all(uploaders);
+        //     console.log("done", data);
+        // } catch (ex) {
+        //     alert(ex.message);
+        // }
+
+
         // creating formdata
         let data = new FormData();
-        data.append("file", this.state.image);
+        this.state.images.map((image) => {
+            // data.append("file", image, 'zahid');
+            data.append(image.name, image);
+        })
 
         let url = 'http://127.0.0.1:5000/trainModel';
 
@@ -28,6 +58,20 @@ class SendImage extends Component {
         } catch (error) {
             console.log("hi", error)
         }
+
+
+        // // creating formdata
+        // let data = new FormData();
+        // data.append("file", this.state.image);
+
+        // let url = 'http://127.0.0.1:5000/trainModel';
+
+        // try {
+        //     const res = await axios.post(url, data)
+        //     console.log(res);
+        // } catch (error) {
+        //     console.log("hi", error)
+        // }
     };
 
     render() {
@@ -36,7 +80,7 @@ class SendImage extends Component {
                 {/* method_1 */}
                 <form encType="multipart/form-data" onSubmit={this.handleSubmit} >
                     <p>
-                        <input type="file" onChange={this.handleImageChange} required />
+                        <input type="file" multiple onChange={this.handleImageChange} required />
                     </p>
                     <input type="submit" value="Submit"></input>
                 </form>
